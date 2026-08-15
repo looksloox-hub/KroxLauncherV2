33406f634b50fc7d148f2ed05084424bc71b515c
looksloox-hub <looksloox@gmail.com>
2026-08-15 10:51:11 +0000

    Add ZalithLauncher code

--- cs/temp-launcher/app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/modloaders/modpacks/models/ModSource.java
+++
@@ -1,8 +0,0 @@
-package net.kdt.pojavlaunch.modloaders.modpacks.models;
-
-import java.io.Serializable;
-
-public abstract class ModSource implements Serializable {
-    public int apiSource;
-    public boolean isModpack;
-}
--- cs/temp-launcher/app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/yggdrasil/InvalidSkinException.java
+++
@@ -1,7 +0,0 @@
-package net.kdt.pojavlaunch.yggdrasil;
-
-public class InvalidSkinException extends Exception {
-    public InvalidSkinException(String message) {
-        super(message);
-    }
-}
